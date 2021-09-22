643485d485965ea8c1722b122cf8d6e38165677e
Pedro Santos Arruda <42540954+pedro-hs@users.noreply.github.com>
2021-09-22 20:42:11 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ docker-compose down &&
 docker-compose up -d postgres &&
 docker-compose up -d rabbitmq &&
 docker-compose up -d notification &&
-[ "$1" == 'local' ] && docker-compose up -d web || python3 manage.py runserver
+[ "$1" == 'local' ] && python3 manage.py runserver || docker-compose up -d web 
